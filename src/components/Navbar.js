cc1c9623760a5ab211f714b515a1eb54fa65b445
Rosie Watson <rosie.watson@holidayextras.com>
2020-05-23 17:26:23 +0100

    Basic React/Webpack set up

---
+++ src/components/Navbar.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Navbar = () => (
+  <h1>Hi</h1>
+)
+
+export default Navbar
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import Navbar from "./components/Navbar"
+
+ReactDOM.render(<Navbar />, document.getElementById('app'))
