e3fa6d8b3a77d95e98c0a2577543aea1b1ebeea0
Rosie Watson <rosie.watson@holidayextras.com>
2020-06-08 14:26:12 +0100

    Redux store working with dev tools chrome extension

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,6 @@
 import { createStore, applyMiddleware } from 'redux'
 import { persistStore, persistReducer } from 'redux-persist'
+import { composeWithDevTools } from 'redux-devtools-extension';
 import thunkMiddleware from 'redux-thunk'
 import storage from 'redux-persist/lib/storage'
 
@@ -13,7 +14,8 @@ const persistConfig = {
 const persistedReducer = persistReducer(persistConfig, reducers)
 
 export default () => {
-  let store = createStore(persistedReducer, initialState, applyMiddleware(thunkMiddleware))
-  let persistor = persistStore(store)
+  const store = createStore(persistedReducer, initialState, composeWithDevTools(applyMiddleware(thunkMiddleware)))
+  const persistor = persistStore(store)
+
   return { store, persistor }
 }
