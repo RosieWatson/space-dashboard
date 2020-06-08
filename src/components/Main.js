8cf955ec04ddc950a823c475195ce22fd5cc5448
Rosie Watson <rosie.watson@holidayextras.com>
2020-06-08 16:55:28 +0100

    Addressing some issues picked up on by a Lighthouse audit

--- src/components/Main.js
+++ src/components/Main.js
@@ -5,7 +5,7 @@ import PictureCard from './PictureCard'
 
 const Main = () => (
   <div id='star-overlay'>
-    <h1 className='text-white m-5'>Space</h1>
+    <h1 className='text-white mx-5 pt-5'>Space</h1>
     <div className='d-flex flex-wrap align-items-start justify-content-center'>
       <PictureCard />
       <PeopleCard />
