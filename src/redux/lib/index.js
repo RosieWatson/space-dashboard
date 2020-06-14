d32106fe88fcbe28ec1f86b865c27e66f6f7057d
Rosie Watson <rosie.watson@holidayextras.com>
2020-06-14 10:04:31 +0100

    Correct caching logic

--- src/redux/lib/index.js
+++ src/redux/lib/index.js
@@ -1,12 +1,13 @@
 const shouldUseCachedData = (state, currentDateTime, obj) => {
-  const timeLastFetched = state[obj].timestamp
-  if (!timeLastFetched) return false
+  const lastFetched = state[obj].timestamp
+  if (!lastFetched) return false
 
   const hasError = state[obj].error
   if (hasError) return false
 
-  const timeNeedsUpdating = new Date(currentDateTime.setMinutes(currentDateTime.getMinutes() + 15))
-  return Date.parse(timeNeedsUpdating) > Date.parse(timeLastFetched)
+  const timeLastFetched = new Date(lastFetched)
+  const timeNeedsUpdating = new Date(timeLastFetched.setMinutes(timeLastFetched.getMinutes() + 15))
+  return Date.parse(timeNeedsUpdating) > Date.parse(currentDateTime)
 }
 
 export default { shouldUseCachedData }
